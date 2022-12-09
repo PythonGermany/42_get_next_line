7cce16942b0b9202170415cb6c89d761f6a23570
PythonGermany <pythongermany@gmail.com>
2022-12-09 12:25:10 +0100

    Bonus start

---
+++ get_next_line_bonus.c
@@ -0,0 +1,126 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 22:34:16 by rburgsta          #+#    #+#             */
+/*   Updated: 2022/10/24 22:34:16 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <unistd.h>
+
+char	*get_next_line(int fd)
+{
+	static char	**stash;
+	char		*line;
+
+	line = 0;
+	if (!stash)
+		stash = (char **)ft_calloc(1024, sizeof(char *));
+	if (stash[fd] && fd >= 0 && BUFFER_SIZE > 0)
+		if (!split_data(&line, stash, stash))
+			return (line);
+	if (!stash[fd] && fd >= 0 && BUFFER_SIZE > 0)
+		read_data(fd, &line, stash);
+	return (line);
+}
+
+void	read_data(int fd, char **line, char **stash)
+{
+	int		ret;
+	char	*buffer;
+
+	buffer = (char *)ft_calloc(1, BUFFER_SIZE + 1);
+	if (!buffer)
+	{
+		*line = 0;
+		return ;
+	}
+	ret = read(fd, buffer, BUFFER_SIZE);
+	while (ret > 0)
+	{
+		buffer[ret] = 0;
+		if (!split_data(line, &buffer, stash))
+			break ;
+		ret = read(fd, buffer, BUFFER_SIZE);
+	}
+	if (buffer && ret < 1)
+		free(buffer);
+	if (*line && ret == -1)
+	{
+		free(*line);
+		*line = 0;
+	}
+	if (!ft_test(stash) && ret < 1)
+		free(stash);
+}
+
+int	split_data(char **line, char **src_data, char **dst_data)
+{
+	char	*buf;
+
+	if (ft_strlen(*src_data, 0) == ft_strlen(*src_data, 1))
+	{
+		ft_cpappend(line, *src_data, 0);
+		if (*dst_data)
+			free(*dst_data);
+		*dst_data = 0;
+		if (line[0][ft_strlen(*line, 0) - 1] == '\n' || !line)
+		{
+			if (*src_data)
+				free(*src_data);
+			return (0);
+		}
+		return (1);
+	}
+	buf = 0;
+	if (!ft_cpappend(line, *src_data, 1))
+		if (ft_cpappend(&buf, *src_data + ft_strlen(*src_data, 1), 0))
+			*line = 0;
+	free(*src_data);
+	*dst_data = buf;
+	return (0);
+}
+
+int	ft_cpappend(char **dst, char *src, int line_break)
+{
+	int		i;
+	char	*buf;
+
+	i = ft_strlen(*dst, line_break) + ft_strlen(src, line_break);
+	buf = (char *)ft_calloc(1, i + 1);
+	if (!buf)
+	{
+		if (*dst)
+			free(*dst);
+		*dst = 0;
+		return (1);
+	}
+	while (i-- > ft_strlen(*dst, line_break))
+		buf[i] = src[i - ft_strlen(*dst, line_break)];
+	i = ft_strlen(*dst, line_break);
+	while (i-- > 0)
+		buf[i] = dst[0][i];
+	if (*dst)
+		free(*dst);
+	*dst = buf;
+	return (0);
+}
+
+int	ft_strlen(char *str, int line_break)
+{
+	int	i;
+
+	i = 0;
+	if (!str)
+		return (0);
+	while (str[i] && (str[i] != '\n' || !line_break))
+		i++;
+	if (str[i] == '\n')
+		i++;
+	return (i);
+}
---
+++ get_next_line_utils_bonus.c
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: rburgsta <rburgsta@student.42heilbronn.    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/10/24 22:34:14 by rburgsta          #+#    #+#             */
+/*   Updated: 2022/10/24 22:34:14 by rburgsta         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+
+void	*ft_calloc(size_t count, size_t size)
+{
+	size_t	i;
+	void	*ptr;
+
+	i = 0;
+	if (((count == (size_t)-1 && size) || (size == (size_t)-1 && count)))
+		return (0);
+	ptr = malloc(count * size);
+	if (ptr)
+		while (i < count * size)
+			*((unsigned char *)ptr + i++) = 0;
+	return (ptr);
+}
+
+int ft_test(char **stash)
+{
+	if (stash[0])
+		return (1);
+	return(0);
+}
