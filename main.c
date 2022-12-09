d05daaf3303203fdb2162e1b4290a0818186f6c0
PythonGermany <pythongermany@gmail.com>
2022-12-09 12:25:07 +0100

    Main.c update

--- main.c
+++ main.c
@@ -6,14 +6,25 @@
 
 int main()
 {
-	int fd1 = open("test1.txt", O_RDONLY);
+	// int fd1 = open("test1.txt", O_RDONLY);
+	// char *str = (char *)1;
+	// while (str)
+	// {
+	// 	str = get_next_line(fd1);
+	// 	printf("%i %s", i, str);
+	// 	free(str);
+	// }
+	// close(fd1);
+
+	int i = 0;
 	char *str = (char *)1;
 	while (str)
 	{
-		str = get_next_line(fd1);
-		printf("%s", str);
+		str = get_next_line(0);
+		printf("%i %s", i++, str);
 		free(str);
 	}
-	close(fd1);
-	system("leaks a.out");
+
+	//system("leaks a.out");
+	return (0);
 }
